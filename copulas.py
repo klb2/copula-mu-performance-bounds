fc18a0b037f69fe6fde95a4cc68a0f4349ced663
KLB <k.besser@tu-bs.de>
2020-03-13 15:36:01 +0100

    Add Gaussian copula CDF

--- copulas.py
+++ copulas.py
@@ -15,16 +15,16 @@ class Copula:
             raise ValueError("All inputs need to be between 0 and 1.")
         return x
     
-    def cdf(self, x):
+    def cdf(self, u):
         pass
     
     def rvs(self, size=1):
         pass
 
 class CopulaFrechetUpper(Copula):
-    def cdf(self, x):
-        x = self._check_input(x)
-        return np.min(x, axis=0)
+    def cdf(self, u):
+        u = self._check_input(u)
+        return np.min(u, axis=0)
 
     def rvs(self, size=1):
         u = np.random.rand(size, 1)
@@ -36,9 +36,9 @@ class CopulaFrechetLower(Copula):
         super().__init__(dim, *args, **kwargs)
         self.dim = 2
     
-    def cdf(self, x):
-        x = self._check_input(x, check_shape=False)
-        return np.maximum(1 - self.dim + np.sum(x, axis=0), 0)
+    def cdf(self, u):
+        u = self._check_input(u, check_shape=False)
+        return np.maximum(1 - self.dim + np.sum(u, axis=0), 0)
 
     def rvs(self, size=1):
         if self.dim != 2:
@@ -49,9 +49,9 @@ class CopulaFrechetLower(Copula):
 
     
 class CopulaProduct(Copula):
-    def cdf(self, x):
-        x = self._check_input(x, check_shape=False)
-        return np.prod(x, axis=0)
+    def cdf(self, u):
+        u = self._check_input(u, check_shape=False)
+        return np.prod(u, axis=0)
     
     def rvs(self, size=1):
         return np.random.rand(size, self.dim)
@@ -62,8 +62,20 @@ class CopulaGaussian(Copula):
         self.cov = cov
         super().__init__(dim=dim, *args, **kwargs)
 
-    def cdf(self, x):
-        return NotImplemented
+    def cdf(self, u):
+        u = np.array(u)
+        u = self._check_input(u)
+        is_meshgrid = (u.ndim == 3)
+        if is_meshgrid:
+            _shape = np.shape(u)[1:]
+            u = np.reshape(u, (-1, int(u.size/len(u)))).T
+        if isinstance(self.cov, (float, int)):
+            cov = np.array([[1, self.cov], [self.cov, 1]])
+        x = stats.norm.ppf(u)
+        _cdf = stats.multivariate_normal.cdf(x, cov=cov, allow_singular=True)
+        if is_meshgrid:
+            _cdf = np.reshape(_cdf, _shape)
+        return _cdf
     
     def rvs(self, size=1, cov=0):
         if self.cov is not None:
--- expectation_bounds.py
+++ expectation_bounds.py
@@ -26,7 +26,6 @@ def _single_upper_sum_rate(lam_x, lam_y, snr_x, snr_y):
                         snr_x=snr_x, snr_y=snr_y)
     expected = integrate.quad(cost_func, 0, 1, args=(lam_x, lam_y, snr_x, snr_y))
     return expected[0]
-
 upper_sum_rate = np.vectorize(_single_upper_sum_rate)
 
 def indep_sum_rate(lam_x, lam_y, snr_x, snr_y):
@@ -36,10 +35,29 @@ def indep_sum_rate(lam_x, lam_y, snr_x, snr_y):
         rate[idx_uneq] = (snr_x*lam_y*exp_times_expi(lam_x/snr_x)-snr_y*lam_x*exp_times_expi(lam_y/snr_y))/(snr_x*lam_y - snr_y*lam_x)
     return -rate/np.log(2)
 
-def upper_mac_rate(lam_x, lam_y, snr_x, snr_y):
-    def cost_func(u, s=1, lam_x=1, lam_y=1):
-            if bound.startswith('max'):
-                _u = 1.-u
-            return func(inv_cdf(u, lam=lam_x), inv_cdf(_u, lam=lam_y), s=s)
-    expected = integrate.quad(cost_func, 0, 1, args=(s,lam_x, lam_y))
+
+
+def _single_upper_mac_rate(lam_x, lam_y, snr):
+    def cost_func(u, snr=1, lam_x=1, lam_y=1):
+        _u = 1.-u
+        return mac_rate(inv_cdf(u, lam=lam_x), inv_cdf(_u, lam=lam_y), snr=snr)
+    expected = integrate.quad(cost_func, 0, 1, args=(snr,lam_x, lam_y))
     return expected[0]
+upper_mac_rate = np.vectorize(_single_upper_mac_rate)
+
+def lower_mac_rate(lam_x, lam_y, snr):
+    s = 1./snr
+    _part1 = np.exp(lam_y*s)*expi(-lam_y*s)
+    _part2 = np.exp((lam_y*lam_x*s)/(lam_x+lam_y))*expi(-lam_x*lam_y*s/(lam_x+lam_y))
+    return (_part1 - _part2)/np.log(2)
+
+def indep_mac_rate(lam_x, lam_y, snr):
+    s = 1./snr
+    if lam_x == lam_y:
+        lam = lam_x
+        expect_xi = 1 + np.exp(lam*s)*(lam*s-1)*expi(-lam*s) + np.log(s)
+        expect_psi = -np.exp(lam*s)*(expi(-lam*s)-np.exp(-lam*s)*np.log(s))
+    else:
+        expect_xi = -(np.exp(lam_x*s)*lam_y*expi(-lam_x*s)-np.exp(lam_y*s)*lam_x*expi(-lam_y*s)+np.log(s)*(lam_x-lam_y))/(lam_y-lam_x)
+        expect_psi = -np.exp(lam_y*s)*(expi(-lam_y*s)-np.exp(-lam_y*s)*np.log(s))
+    return (expect_xi - expect_psi)/np.log(2)
